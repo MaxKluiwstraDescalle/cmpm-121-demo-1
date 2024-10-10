43d008b03dcc4ab7e2f1fdb8f17076eb0df3b648
Max-Emilien Kluiwstra-Descalle <mkluiwst@ucsc.edu>
2024-10-10 11:47:54 -0700

    Step 5 Complete :3

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 let counter: number = 0;
+let growth : number = 0;
+let rate: number = 0;
 
 const gameName = "Miles Biked";
 document.title = gameName;
@@ -18,35 +20,59 @@ app.append(counterDisplay);
 
 // Create the button element
 const button = document.createElement("button");
+const buttonUp = document.createElement("button");
 
 // Set the button's text
 button.textContent = "Click Me 🚴";
+buttonUp.textContent = "Buy Teammates!";
+
+buttonUp.style.top = "600px";
+buttonUp.style.left = "550px"
 
 // Add an event listener to handle clicks
 button.addEventListener("click", () => {
   counter += 1;
-  counterDisplay.innerHTML = `Counter: ${counter}`;
+  counterDisplay.innerHTML = `Miles: ${counter}`;
 });
 let lastTime = 0;
 let fps = 60;
 let increment = 0;
 
+buttonUp.addEventListener("click", () => {
+    if(counter>= 10){
+        counter-= 10;
+        rate += 1;
+        growthRate(rate);
+        
+    }
+  });
+
+buttonUp.disabled= true;
+
+function updateButtonState(){
+    buttonUp.disabled = counter < 10;
+}
+
 function updateCounter(currentTime: number) {
   const deltaTime = currentTime - lastTime;
   fps = 1000 / deltaTime;
-  increment = 1 / fps;
+  increment = growth / fps;
   counter += increment;
-  counterDisplay.innerHTML = `Counter: ${counter.toFixed(2)}`;
+  counterDisplay.innerHTML = `Miles: ${counter.toFixed(2)}`;
 
   lastTime = currentTime;
+
+  updateButtonState();
   requestAnimationFrame(updateCounter);
 }
 
-/*setInterval(() => {
-  counter += 1;
-  counterDisplay.innerHTML = `Counter: ${counter}`;
-}, 1000);*/
+function growthRate(newRate: number){
+    growth = newRate
+}
+
 
 // Append the button to the body (or any other container element)
 document.body.appendChild(button);
+document.body.appendChild(buttonUp);
 requestAnimationFrame(updateCounter);
+
