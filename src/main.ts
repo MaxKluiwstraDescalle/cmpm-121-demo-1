eb18804a9b6d7d667709d66e518422bb72ed3d3c
Max-Emilien Kluiwstra-Descalle <mkluiwst@ucsc.edu>
2024-10-27 16:23:37 -0700

    D1.c Done, improve code legibility

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 let counter: number = 0;
 let growth: number = 0;
-const baseGrowthRate = 1.15
+const baseGrowthRate = 1.15;
 const baseCost = 10;
 
 function updateButtonState(button: HTMLButtonElement, item: autoClickerButton) {
@@ -68,9 +68,9 @@ interface autoClickerButton {
 }
 
 const availableItems: autoClickerButton[] = [
-  { name: "Teammate", cost: 1*baseCost, rate: 0.1 },
-  { name: "Coach", cost: 10*baseCost, rate: 2 },
-  { name: "Team", cost: 100*baseCost, rate: 50 },
+  { name: "Teammate", cost: 1 * baseCost, rate: 0.1 },
+  { name: "Coach", cost: 10 * baseCost, rate: 2 },
+  { name: "Team", cost: 100 * baseCost, rate: 50 },
 ];
 
 const purchaseCount: { [key: string]: number } = {
@@ -84,8 +84,6 @@ const purchaseCounters: { [key: string]: HTMLSpanElement } = {
   Coach: document.createElement("span"),
   Team: document.createElement("span"),
 };
-
-
 
 const gameName = "Miles Biked";
 document.title = gameName;
@@ -122,12 +120,9 @@ const buttonContainer = document.createElement("div");
 buttonContainer.id = "buttonContainer";
 app.appendChild(buttonContainer);
 
-
-
 createItemButtons(availableItems);
 let lastTime = 0;
 let fps = 60;
 let increment = 0;
-
 
 requestAnimationFrame(updateCounter);
